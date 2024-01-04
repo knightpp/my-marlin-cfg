b812a3029f140db97eb8ba2fa1e1725763cebb31
Danylo Kondratiev <knightpp@proton.me>
2024-01-04 21:16:18 +0200

    Use FOD to fetch pio deps

--- marlin.nix
+++ marlin.nix
@@ -7,11 +7,7 @@
   pioEnv ? "trigorilla_pro",
   rev,
   hash,
-}:
-stdenv.mkDerivation {
-  pname = "marlin-firmware-${pioEnv}";
-  version = rev;
-
+}: let
   src = fetchFromGitHub {
     owner = "MarlinFirmware";
     repo = "Marlin";
@@ -19,25 +15,96 @@ stdenv.mkDerivation {
     sha256 = hash;
   };
 
-  nativeBuildInputs = [platformio];
+  deps = stdenv.mkDerivation {
+    pname = "marlin-firmware-${pioEnv}-deps";
+    version = rev;
+    inherit src;
 
-  configurePhase = ''
-    cp ${configs}/* Marlin/
-  '';
+    nativeBuildInputs = [platformio];
 
-  buildPhase = ''
-    pio run --environment ${pioEnv}
-  '';
+    configurePhase = ''
+      runHook preConfigure
 
-  installPhase = ''
-    cp .pio/build/${pioEnv}/firmware.* $out
-  '';
+      export HOME=$(mktemp -d)
 
-  meta = with lib; {
-    description = "Marlin is an optimized firmware for RepRap 3D printers based on the Arduino platform.";
-    license = licenses.gpl3;
-    maintainers = with maintainers; [knightpp];
-    platforms = platforms.linux;
-    homepage = "https://github.com/MarlinFirmware/Marlin";
+      runHook postConfigure
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+
+      pio pkg install --project-dir . --environment ${pioEnv}
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      find $HOME/.platformio/ -type d -name __pycache__ -prune -exec rm -rf {} \;
+      mkdir -p $out
+      cp -r --reflink=auto $HOME/.platformio/{packages,platforms} $out/
+
+      runHook postInstall
+    '';
+
+    dontFixup = true;
+
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = "sha256-AK9kBoDLY5i1vC6sEUbJ855rHUXim4LIgvHsHoAhk4o=";
   };
-}
+in
+  stdenv.mkDerivation {
+    pname = "marlin-firmware-${pioEnv}";
+    version = rev;
+
+    src = fetchFromGitHub {
+      owner = "MarlinFirmware";
+      repo = "Marlin";
+      inherit rev;
+      sha256 = hash;
+    };
+
+    nativeBuildInputs = [platformio];
+
+    configurePhase = ''
+      runHook preConfigure
+
+      export HOME=$(mktemp -d)
+      mkdir -p $HOME/.platformio
+      cp -r --reflink=auto ${deps}/* $HOME/.platformio
+      chmod -R u+rw $HOME/.platformio
+
+      cp ${configs}/* Marlin/
+
+      runHook postConfigure
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+
+      pio run --environment ${pioEnv}
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out
+      cp $PWD/.pio/build/${pioEnv}/firmware.* $out
+
+      runHook postInstall
+    '';
+
+    passthru = {inherit deps;};
+
+    meta = with lib; {
+      description = "Marlin is an optimized firmware for RepRap 3D printers based on the Arduino platform.";
+      license = licenses.gpl3;
+      maintainers = with maintainers; [knightpp];
+      platforms = platforms.linux;
+      homepage = "https://github.com/MarlinFirmware/Marlin";
+    };
+  }
